bbcf3599d2bc84890dd39608c009d5fb6874a88a
Graeme Sutherland <grasuth@nodestone.com>
2012-07-19 17:38:01 +0100

    license, setup.py (untested)

---
+++ setup.py
@@ -0,0 +1,37 @@
+from os.path import join
+import re
+from setuptools import setup, find_packages
+
+
+# dynamically pull the version from django_ace/__init__.py
+version = re.search('^__version__ = "(.+?)"$',
+                    open(join('django_prod_calendar', 
+                                '__init__.py')).read(), 
+                                                re.MULTILINE).group(1)
+
+setup(
+    name='django-prod-calendar',
+    version=version,
+    description='A production calendar and demand scheduler for django',
+    long_description=open('README.rst').read(),
+
+    author='Graeme Sutherland',
+    author_email='grasuth@nodestone.com',
+    license="Simplified BSD",
+    url='https://github.com/grasuth/django-prod-calendar',
+
+    packages=find_packages(exclude=["example", "example.*"]),
+    include_package_data=True,
+    install_requires=['Django'],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+        ],
+    )
+
