b9b562298a63cbdc17c31ba3f5ffd13f40804b1d
Graeme Sutherland <grasuth@nodestone.com>
2012-07-20 11:01:16 +0100

    Set up defaults table

--- prod_calendar/models.py
+++ prod_calendar/models.py
@@ -1,29 +1,56 @@
 from django.db import models
+from django.utils.translation import ugettext as _
 
 """
 Production calendar models.
 
 Core concepts are:
 
-    * There are slots that can have a capacity.
-    * Slots have a fixed time period (day, week, month)
+    * There is a default number of places available per slot.
+    * There are slots that can have a number of spaces.
+    * Slots have a fixed length, to determine when the next slot can start
     * Slots are allocated lazily: the don't exist until referenced, then
-      that slot is created.
-    * Slots can be filled with a recipe of different types of item
+      that slot is created. This makes sure we have a calandar for
+      some number of periods in the future.
+    * The Value of a space is determined by the collection of SpaceValues.
+    * Lower value spaces are assigned first.
 
 """
 
 
 class Defaults(models.Model):
-    available = models.IntegerField()
+    """
+    Defines defaults for slots not yet created
+
+    We want this to be user-modifiable, so there ought to be just
+    one record in this table. We enforce this in admin screens and
+    model save and delete.
+
+    """
+    spaces = models.IntegerField()
+
+    def save(self):
+        """
+        Force id to 1, to keep single row in table
+
+        """
+        self.id = 1
+        super(Defaults, self).save()
+
+    def delete(self):
+        """
+        Remove ability to delete single row.
+
+        """
+        pass
 
 
 class Slot(models.Model):
-    number = IntegerField()
-    available = models.IntegerField()
-    used = models.IntegerField()
+    start_date = models.DateField()
+    spaces = models.IntegerField()
+    used_spaces = models.IntegerField()
 
 
-class SlotValue(models.Model):
-    quantity = models.IntegerField()
+class SpaceValue(models.Model):
+    spaces = models.IntegerField()
     value = models.DecimalField()
